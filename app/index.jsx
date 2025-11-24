176b21c082caa9c3e147298c21adc2444ce61f0b
APigott1 <pigster99@gmail.com>
2025-11-24 14:02:50 +0000

    fix npm dependency conflict

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-const home = () => {
+const Home = () => {
   return (
     <View>
-      <Text>home</Text>
+      <Text>Home</Text>
     </View>
-  )
-}
+  );
+};
 
-export default home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
