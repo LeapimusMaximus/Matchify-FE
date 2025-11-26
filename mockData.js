1758f2be6440b22a4d99e32501b6b6e54bf3bc04
srmacpherson <srmacpherson96@gmail.com>
2025-11-26 10:51:56 +0000

    add mock data for a user. add promfile image and match buttons to the matching feed

---
+++ mockData.js
@@ -0,0 +1,58 @@
+const Users = [
+  {
+    spotifyId: "1234567890abcdef",
+    displayName: "Jane Doe",
+    email: "janedoe@example.com",
+    profileImage:
+      "https://picsum.photos/200",
+    profileSongs: [
+      {
+        trackId: "3n3Ppam7vgaVa1iaRUc9Lp",
+        trackName: "Blinding Lights",
+        artistName: "The Weeknd",
+        albumArt:
+          "https://i.scdn.co/image/ab67616d0000b273e1c1c1c1c1c1c1c1c1c1c1c1",
+        previewUrl:
+          "https://p.scdn.co/mp3-preview/1234567890abcdef1234567890abcdef123456?cid=774b29d4f13844c495f206cafdad9c86",
+      },
+      {
+        trackId: "7ouMYWpwJ422jRcDASZB7P",
+        trackName: "Watermelon Sugar",
+        artistName: "Harry Styles",
+        albumArt:
+          "https://i.scdn.co/image/ab67616d0000b273abcdef1234567890abcdef12",
+        previewUrl:
+          "https://p.scdn.co/mp3-preview/abcdef1234567890abcdef1234567890abcdef12?cid=774b29d4f13844c495f206cafdad9c86",
+      },
+      {
+        trackId: "0VjIjW4GlUZAMYd2vXMi3b",
+        trackName: "Save Your Tears",
+        artistName: "The Weeknd",
+        albumArt:
+          "https://i.scdn.co/image/ab67616d0000b2731234567890abcdef12345678",
+        previewUrl:
+          "https://p.scdn.co/mp3-preview/7890abcdef1234567890abcdef1234567890abcd?cid=774b29d4f13844c495f206cafdad9c86",
+      },
+      {
+        trackId: "4NRXx6U8ABQ4zj4xFz4lU2",
+        trackName: "Levitating",
+        artistName: "Dua Lipa",
+        albumArt:
+          "https://i.scdn.co/image/ab67616d0000b273abcdefabcdef1234567890ab",
+        previewUrl:
+          "https://p.scdn.co/mp3-preview/abcdefabcdef1234567890abcdef1234567890ab?cid=774b29d4f13844c495f206cafdad9c86",
+      },
+      {
+        trackId: "1a2b3c4d5e6f7g8h9i0j",
+        trackName: "Peaches",
+        artistName: "Justin Bieber",
+        albumArt:
+          "https://i.scdn.co/image/ab67616d0000b2731234abcd5678ef901234abcd",
+        previewUrl:
+          "https://p.scdn.co/mp3-preview/1234abcd5678ef901234abcd5678ef901234abcd?cid=774b29d4f13844c495f206cafdad9c86",
+      },
+    ],
+  },
+];
+
+module.exports = Users;
